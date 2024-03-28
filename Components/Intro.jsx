e8f2fadeec00abe3f7c706927a0360437246e023
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-28 15:08:32 +0000

    start--button styled

--- Components/Intro.jsx
+++ Components/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
       <h3> Ready to test your knowledge? </h3>
       <h3> Welcome aboard the Quizzical app! </h3> <img src="/src/assets/quiz.png" alt="logo" width="30%" />
       <br />
-        <button className="start--button"> <h2>Start Quiz </h2></button>
+        <button className="start--button"> <h3>Start Quiz </h3></button>
     </div>
   )
 }
