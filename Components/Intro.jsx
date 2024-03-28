51b34708984686c4ded3c0d3a229a7db5bbbef12
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-28 13:26:26 +0000

    intro page, introduction added

---
+++ Components/Intro.jsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+  return (
+    <div>
+      <h1> Quizzical </h1>
+      <p> Ready to test your knowledge and tickle your brain? </p>
+      <p> Welcome aboard the Quizzical app! </p> <img src="/src/assets/quiz.png" alt="logo" width="50%" />
+    </div>
+  )
+}
