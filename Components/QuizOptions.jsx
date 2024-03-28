0865a44c6f5c567c3fa552280bfb214078dab8f8
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-28 16:03:16 +0000

    worked on the Quiz options component

---
+++ Components/QuizOptions.jsx
@@ -0,0 +1,8 @@
+export default function QuizOptions() {
+  return (
+    <main>
+      <h3> Select your Category </h3>
+      <h3> Select Difficulty level </h3>
+    </main>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import Intro from '../Components/Intro'
+import QuizOptions from '../Components/QuizOptions.jsx'
 import './App.css'
 
 function App() {
+  const [start, setStart] = useState(false)
+
+  function begin() {
+    setStart(prevStat => !prevStat)
+    // when you click begin quiz button
+    // you should see the QuizOptions component
+    // instead of the Intro component
+    // you can use the useState hook to manage this
+    // and the clickHandler function to change the state
+    // of the component
+  }
   return (
     <main>
-      <Intro />
+      {<Intro clickHandler={begin} /> && start === false}
+      {start ? <QuizOptions /> : null}
     </main>
   )
 }
