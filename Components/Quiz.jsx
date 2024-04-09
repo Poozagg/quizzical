9d77d04bd23879ab2b6b096a127b8f1c1a62f15f
Pooja Gurung <pooja_grg07@hotmail.com>
2024-04-09 10:08:58 +0100

    horizontal line added css and made the questions h2 instead of h3

--- Components/Quiz.jsx
+++ Components/Quiz.jsx
@@ -1,32 +1,52 @@
 import PropTypes from 'prop-types'
 import shuffleArray from 'shuffle-array'
 import { decode } from 'html-entities'
+import { useState } from 'react'
 
 export default function Quiz(props) {
-  // --! onClick function which will display the questions and answer options !--
-  // const quizQandA = props.quizQuestions.map((item) => {
-  //   return (
-  //     <Quiz
-  //       key={item.id}
-  //       question={item.question}
-  //       answers={item.answers}
-  //     />
-  //   )
-  // })
+  const [userAnswers, setUserAnswers] = useState([])
+
+  // --! function to handle the submit button !--
+  function handleSubmit(e) {
+    // e.defaultPrevent()
+    console.log(e.target.name, e.target.value)
+    // setUserAnswers({
+    //   ...userAnswers,
+    //   [e.target.name]: e.target.value
+    // })
+  }
+
+  // --! function to handle the change in the answer selected by User !--
+  // function onOptionChange(e) {
+  //   console.log(e.target.name, e.target.value)
+  //   setUserAnswers({
+  //     ...userAnswers,
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
+
   // --! function which will display the questions and answer options !--
   const questionAndAnswers = props.quizQuestions.map((item) => {
     const answers = shuffleArray(item.answers)
     return (
       <div key={item.id}>
-        <h3>{decode(item.question)}</h3>
+        <h2>{decode(item.question)}</h2>
         <ul className="radio--button--styling">
           {answers.map((answer, index) => (
             <li className="Quiz--Answer--Options" key={index}>
-              <input type='radio' id={answer} name={item.id} value={answer} />
+              <input
+                type='radio'
+                id={answer}
+                name={item.id}
+                value={answer}
+                // checked={userAnswers[item.id] === answer}
+                // onChange={onOptionChange}
+              />
               {/* decoding the answer to display ONLY!! */}
               <label htmlFor={answer}>{decode(answer)}</label>
             </li>
           ))}
+          <hr className="horizontal--line" />
         </ul>
       </div>
     )
@@ -38,7 +58,10 @@ export default function Quiz(props) {
       <div>
         {questionAndAnswers}
       </div>
-        <button className='submit--answer--button'>
+        <button
+          className='submit--answer--button'
+          onClick={handleSubmit}
+        >
           Submit Answer
         </button>
     </div>
